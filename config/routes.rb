873c39dbeae4a3e2ad9e08e80f20df76ab1ebdf9
satou-yuuki <satouwork0316@gmail.com>
2019-03-24 14:08:43 +0900

    collection_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+  root 'items#index'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
     :passwords => 'users/passwords'
   }
   resources :users, :only => [:index, :show]
-=======
-
-
-
-
-
-
-
-  root 'items#index'
-
   resources :items
->>>>>>> master
 end
