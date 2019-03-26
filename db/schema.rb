166faf21f4c170b7cc18e3a413086e1cfbc204b8
satou-yuuki <satouwork0316@gmail.com>
2019-03-26 19:06:02 +0900

    AddColumnToUsers_for_identification
    本人情報の登録用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_090328) do
+ActiveRecord::Schema.define(version: 2019_03_26_071616) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,21 @@ ActiveRecord::Schema.define(version: 2019_03_22_090328) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "first_name_kana"
+    t.string "last_name_kana"
+    t.integer "birth_year"
+    t.integer "birth_month"
+    t.integer "birth_day"
+    t.integer "postal_code"
+    t.string "prefecture"
+    t.string "city"
+    t.string "block"
+    t.string "building"
+    t.string "phone_number"
+    t.string "profit"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
