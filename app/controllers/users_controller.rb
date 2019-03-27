52da9120805bbc238499a13a555407c3393270ff
satou-yuuki <satouwork0316@gmail.com>
2019-03-27 13:17:33 +0900

    コントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_new_user, only: [:identification, :card]
-  layout 'user'
+  before_action :set_user, only: [:show, :identification, :card]
   def index
     @user = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def identification
@@ -19,10 +17,6 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def set_new_user
-    @user = User.new
   end
 
 end
