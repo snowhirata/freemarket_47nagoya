6d2486f930cf0441912002b210fb255c066ab060
satou-yuuki <satouwork0316@gmail.com>
2019-03-27 14:46:31 +0900

    ルートのハッシュロケット修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   root 'items#index'
 
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions',
-    :passwords => 'users/passwords'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
   }
 
-  resources :users, :only => [:index, :show] do
+  resources :users, only: [:index, :show] do
     member do
       get 'identification'
       get 'register_card'
     end
   end
-  
+
   resources :items  do
     resources :sold, only: [:index, :show]
   end
