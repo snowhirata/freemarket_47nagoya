8df7c1590f8e8b8a6a5c86b94ee3aeeec06eee93
satou-yuuki <satouwork0316@gmail.com>
2019-04-02 13:44:24 +0900

    Oauth認証機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_01_102035) do
+ActiveRecord::Schema.define(version: 2019_04_02_035246) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 2019_04_01_102035) do
     t.integer "security_code"
     t.string "exp_month"
     t.string "exp_year"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
