fcf96b858317f810d8e6bfeef164f060a670e0c8
tetsu-26 <heiwa326427@gmial.com>
2019-04-03 21:03:39 +0900

    エラーハンドリング実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,12 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :category_items
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  validates :name, presence: true
+  validates :state, presence: true
+  validates :prefecture_id, presence: true
+  validates :ship_charge, presence: true
+  validates :ship_method, presence: true
+  validates :ship_date, presence: true
+  validates :price, presence: true
 end
