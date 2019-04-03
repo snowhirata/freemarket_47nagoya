401d3c3a9c15a0ef15d6863f801ea43e2fe3c845
satou-yuuki <satouwork0316@gmail.com>
2019-04-03 13:05:41 +0900

    Binding.pry削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,7 +15,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    binding.pry
     if @user.persist
       sign_in_and_redirect @user, event: :authentication
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,6 @@ class User < ApplicationRecord
         )
         credential.save
       end
-      binding.pry
       user
   end
 end
