de22f99fb4c88f10431b19a0410100c2e39e7733
snowhirata <yukihirata@hirataakinoMacBook-puro.local>
2019-04-04 10:54:03 +0900

    軽微な修正

--- db/migrate/20190322090328_devise_create_users.rb
+++ db/migrate/20190322090328_devise_create_users.rb
@@ -59,6 +59,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     add_column :users, :card_number, :integer
     add_column :users, :exp_month, :string
     add_column :users, :exp_year, :string
-    
+
   end
 end
