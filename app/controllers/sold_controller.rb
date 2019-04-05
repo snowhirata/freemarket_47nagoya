ca2f34310bef83217a7339e08eb29d7c5ecc1221
snowhirata <yukihirata@hirataakinoMacBook-puro.local>
2019-04-05 19:10:26 +0900

    商品購入確認のデータ反映

--- app/controllers/sold_controller.rb
+++ app/controllers/sold_controller.rb
@@ -1,8 +1,14 @@
 class SoldController < ApplicationController
 
   def index
+    @item = Item.find(item_params[:item_id])
   end
 
   def show
+  end
+
+  private
+  def item_params
+    params.permit(:item_id)
   end
 end
