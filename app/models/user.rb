8f6ad8578595af1acabf871bb90e6a38b82804b4
satou-yuuki <satouwork0316@gmail.com>
2019-04-05 13:51:26 +0900

    ログイン機能の修正と変更
    ・メールアドレスでの登録
    ・Facabookでの登録
    ・Googleでの登録
    以上は確認しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,6 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
@@ -47,5 +45,6 @@ class User < ApplicationRecord
         )
         credential.save
       end
+      user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,6 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :user_steps, only: [:index, :show, :update]
+
 end
