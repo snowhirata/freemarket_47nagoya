bdffa26ecb122cf171f1918ac251364c478b8d38
satou-yuuki <satouwork0316@gmail.com>
2019-04-09 17:35:34 +0900

    不要な記述を削除

--- app/assets/javascripts/payjp2.js
+++ app/assets/javascripts/payjp2.js
@@ -1,20 +1,15 @@
 
 $(function() {
-  // var form = $("charge-form"),
-  //     number = form.find('input[name="number"]'),
-  //     cvc = form.find('input[name="cvc"]'),
-  //     exp_month = form.find('select[name="exp_month"]'),
-  //     exp_year = form.find('input[name="exp_year"]');
+
    Payjp.setPublicKey('pk_test_61e818d4f22147be9f208a2b');
   $("#ivent-button3").click(function(e) {
-    // form.find("input[type=submit]").prop("disabled", true);
+
     console.log('開始')
     e.preventDefault();
 
     card_number = $('#card_number').val()
     exp_month = $('#exp_month').val()
-    // exp_year = $('#exp_year').val()
-    exp_year = 2020
+    exp_year = $('#exp_year').val()
     security_code = $('#security_code').val()
 
     card_number = Number(card_number);
