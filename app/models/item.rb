2f113414475af5e701712ab0c49b643d555abaeb
satou-yuuki <satouwork0316@gmail.com>
2019-04-15 12:26:57 +0900

    デプロイ

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,6 @@ class Item < ApplicationRecord
   validates :ship_method, presence: true
   validates :ship_date, presence: true
   validates :price, presence: true
-  validates :category, presence: true
+  validates :category_id, presence: true
   validates :user_id, presence: true
 end
